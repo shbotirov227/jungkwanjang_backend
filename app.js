0579458f833e41f2609a51dae7a5c2daf9ad98b9
shbotirov227 <shbotirov227@gmail.com>
2025-04-29 23:56:17 +0500

    fix

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "https://jungkwanjang.uz/" }));
+app.use(cors());
 app.use(express.json());
 
 app.post("/send-message", async (req, res) => {
